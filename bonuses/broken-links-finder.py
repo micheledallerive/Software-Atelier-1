72503f880d6b7e17d12373f0c6af7b4b8ceed054
Michele Dalle Rive <dallem@usi.ch>
2021-11-20 12:55:39 +0100

    Completed bonus exercises:  - Removed example html files in ./bonuses/  - Uploaded the first bonus problem (./bonuses/broken-links-finder.py)  - Created a requirements.txt file (./bonuses/requirements.txt) to install the required packages
    Created the top menu on the pages:
     - Changed the menu icon from .svg file to a CSS element made by span (./src/css/biography.css) in order to allow animation
     - Completed the file ./src/js/menu.js that makes the menu toggler work
     - Downloaded jQuery (./src/js/jquery.min.js) and removed the CDN to make it work offline too.
    
    
    Former-commit-id: 4ddab3087f2dd94389654ebf83c776627166fcad

---
+++ bonuses/broken-links-finder.py
@@ -0,0 +1,47 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+from glob import glob
+from urllib.parse import urlparse
+
+def is_absolute(url):
+    return bool(urlparse(url).netloc)
+
+def find_broken_links(path, html):
+    invalid_urls = []
+    soup = BeautifulSoup(html,features="html.parser")
+    for link in soup.find_all("a"):
+        url = link.get("href")
+        if url=="#": continue
+        if is_absolute(url):
+            try:
+                resp = requests.get(url)
+                code = resp.status_code
+                if code!=200:
+                    invalid_urls.append(url)
+            except requests.exceptions.RequestException as e:
+                invalid_urls.append(url)
+        else:
+            base_url = soup.find("base").get("href")
+            joined_path = os.path.join(os.path.dirname(path), base_url, url)
+            if not os.path.exists(joined_path):
+                invalid_urls.append(url)
+
+    urls_with_line = {}
+    for i,line in enumerate(str(soup).split("\n")):
+        for invalid in invalid_urls:
+            if invalid in line:
+                urls_with_line[invalid]=i+1
+    
+    return urls_with_line
+
+files = glob("**/*.html", recursive=True)
+for f in files:
+    print(f)
+    content = open(f, encoding="utf8")
+    broken_links = find_broken_links(f, content)
+    if len(broken_links)==0:
+        print("- No broken link found")
+    for link in broken_links.keys():
+        print("-", "Line", broken_links[link], "->", link, "is invalid")
+    print()
