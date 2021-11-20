44c8537bef9bd89b88176cc60358cc7a609eefef
Michele Dalle Rive <dallem@usi.ch>
2021-11-20 10:29:11 +0100

    Created the Javascript file for the menu (./src/js/menu.js) Started the creation of the menu in Blum biography (./src/html/biographies/blum.html) Minor changes to the dark JS file (./src/js/dakr.js)
    Former-commit-id: bcfdc444894383a581648b83df7d270583505939

--- src/js/dark.js
+++ src/js/dark.js
@@ -22,7 +22,6 @@ function changeColors(isLightMode){
     document.documentElement.style
     .setProperty(k, v);    
   });
-  
 }
 
 let savedLightMode = localStorage.getItem("lightMode");
@@ -43,29 +42,3 @@ $(document).ready(function(){
     });
   });
 });
-
-/*
-if (document.getElementById("content").classList.contains('light-mode')){
-  document.getElementById("content").classList.toggle('light-mode');
-  document.getElementById("svg").classList.add('svg-dark');
-  document.getElementById("svg").classList.remove('svg-light');
-  document.getElementById("switch").classList.add('svg-dark');
-  document.getElementById("switch").classList.remove('svg-light');
-  document.getElementById("main").classList.add('clr-light');
-  document.getElementById("main").classList.remove('clr-dark');
-  document.getElementById("foo").classList.add('clr-light');
-  document.getElementById("foo").classList.remove('clr-dark');
-  document.getElementById("switch").src="images/lightmode.svg";
-} else {
-  document.getElementById("content").classList.toggle('light-mode')
-  document.getElementById("svg").classList.add('svg-light');
-  document.getElementById("svg").classList.remove('svg-dark');
-  document.getElementById("switch").classList.add('svg-light');
-  document.getElementById("switch").classList.remove('svg-dark');
-  document.getElementById("main").classList.add('clr-dark');
-  document.getElementById("main").classList.remove('clr-light');
-  document.getElementById("foo").classList.add('clr-dark');
-  document.getElementById("foo").classList.remove('clr-light');
-  document.getElementById("switch").src="images/darkmode.svg";
-}
-*/
