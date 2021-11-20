532ef397e924763486aba2c1d3093f1f3f91def0
Alessandro Zanzi <zanzia@usi.ch>
2021-11-20 15:25:16 +0100

    added landing page at src/index.html with relative src/css/home.css and src/js/home.js for animations. created directory src/lib for storing external libraries, added required libraries.
    Former-commit-id: 93133c90579b9612208fd0829d2e928f0cd29819

---
+++ src/js/home.js
@@ -0,0 +1,121 @@
+// DO NOT EDIT,
+// ONLY AFFECTS HOMEPAGE
+
+const controller = new ScrollMagic.Controller();
+
+const tween = new TimelineMax();
+
+// Hero section
+
+tween.to(".ta-title", 1, { xPercent: window.innerWidth * 0.065, left: "0%" });
+tween.to(".logo", 1, { xPercent: - window.innerWidth * 0.05, left: "0%" }, 0);
+tween.to(".logo", 1, { opacity: 1 }, "0.25");
+tween.to(".t-cta", 1, { opacity: 1 }, "0.25");
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: ".hero",
+    triggerHook: "onLeave",
+    duration: "300%"
+}).setPin(".hero")
+    .setTween(tween)
+    // .addIndicators()
+    .addTo(controller);
+
+// Project section
+
+// // counter animation
+let counters = document.querySelectorAll('.counter');
+let speed = 400;
+
+
+new ScrollMagic.Scene({
+    triggerElement: ".project",
+    // triggerHook: "onLeave",
+    duration: "100%"
+}).on("start", function() {
+    counters.forEach(counter => {
+        const updateCounter = () => {
+            const target = counter.getAttribute('target');
+            const count = +counter.innerText;
+
+            const i = target / 35;
+
+            if (count < target) {
+                counter.innerText =  Math.floor(count + 1);
+                setTimeout(updateCounter, 1);
+            } else {
+                counter.innerText =  target;
+            }
+        }
+        updateCounter();
+    })
+
+})
+    // .addIndicators()
+    .addTo(controller);
+
+
+new ScrollMagic.Scene({
+    triggerElement: ".project",
+    triggerHook: "onLeave",
+    duration: "50%"
+}).setPin(".project")
+    // .addIndicators()
+    .addTo(controller);
+
+
+// Dark mode section
+
+new ScrollMagic.Scene({
+    triggerElement: ".dark",
+    triggerHook: "onLeave",
+    duration: "50%"
+}).setPin(".dark")
+    // .addIndicators()
+    .addTo(controller);
+
+function changeToDark() {
+    $("#darkModeSectionIcon").attr("src","images/darkmode-o.svg");
+    $("#darkModeSectionText").attr("src","images/dark-o.svg");
+}
+
+new ScrollMagic.Scene({
+    triggerElement: ".dark",
+    triggerHook: "onLeave",
+    offset: "-200"
+}).on("start", function () {
+    let duration = 400;
+    $("#darkModeSectionIcon").fadeOut(duration, function () {
+        changeToDark();
+        $("#darkModeSectionIcon").fadeIn(duration);
+    });
+}).setClassToggle(".dark", "color-d")
+    .reverse(false)
+    // .addIndicators()
+    .addTo(controller);
+
+
+// Explore section
+
+new ScrollMagic.Scene({
+    triggerElement: ".explore",
+    triggerHook: "onLeave",
+    offset: "-300",
+    duration: "40%",
+}).setTween(new TimelineMax().to(".explore-text", 1, { opacity: 1 }))
+    // .addIndicators()
+    .addTo(controller);
+
+// On end of scroll redirect to page x
+let redirect = "html/timeline.html"; //<- adjust page path
+
+window.addEventListener("scroll", () => {
+    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+
+    if (scrolled === scrollable) {
+        setInterval(() => {
+            window.location.href = redirect;
+        }, 1000);
+    }
+})
